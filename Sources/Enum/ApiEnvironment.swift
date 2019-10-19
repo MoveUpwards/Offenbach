90038fbc57ebec7f624f6786a22a226248452cc6
Loïc GRIFFIE <lgriffie@mbp-de-loic.lan>
2019-10-19 17:26:24 +0200

    Add ApiEnvironment ENUM + Improve Config protocol

---
+++ Sources/Enum/ApiEnvironment.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Loïc GRIFFIE on 19/10/2019.
+//
+
+import Foundation
+
+public enum ApiEnvironment {
+    case develop
+    case staging
+    case production
+}
