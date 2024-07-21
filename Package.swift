9f866898f1f81ce07d53b1d9c087432c662c5855
Loic GRIFFIE <loic@griffie.fr>
2024-07-21 17:39:55 +0200

    [feat] Update Alamofire

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Offenbach",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v12),
         .macOS(.v10_13)
     ],
     products: [
